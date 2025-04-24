d0edb1fbc4ee1d6aebfb4370694cf8e7f31af05b
pdetoni <pdpereira2003@gmail.com>
2025-04-24 15:26:32 -0300

    searching user by id

--- src/main/java/com/pdetoni/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pdetoni/workshopmongo/resources/UserResource.java
@@ -7,6 +7,7 @@ import com.pdetoni.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,14 @@ public class UserResource {
         List<User> users = service.findAll();
         List<UserDTO> listDto = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return  ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
 }
