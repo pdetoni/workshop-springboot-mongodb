2846574ec0e0ae893fa6d588e575cbfb7e8af2b3
pdetoni <pdpereira2003@gmail.com>
2025-04-24 16:17:22 -0300

    user deletion with delete

--- src/main/java/com/pdetoni/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/pdetoni/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -14,7 +14,7 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 
         HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "User not found", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
 
     }
