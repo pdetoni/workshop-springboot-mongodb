8ae6d2049443560b69cc5b6e8960946ea1bb2320
pdetoni <pdpereira2003@gmail.com>
2025-04-28 09:23:09 -0300

    referencing user posts

--- src/main/java/com/pdetoni/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pdetoni/workshopmongo/config/Instantiation.java
@@ -41,5 +41,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, "Bom dia.", sdf.parse("21/03/2018"), "Acordei feliz hoje", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
