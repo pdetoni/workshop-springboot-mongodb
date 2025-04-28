42c33cdd2a4b81a1d235e38664f18201bab34785
pdetoni <pdpereira2003@gmail.com>
2025-04-28 11:22:27 -0300

    simple query with @Query

--- src/main/java/com/pdetoni/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pdetoni/workshopmongo/repository/PostRepository.java
@@ -2,10 +2,14 @@ package com.pdetoni.workshopmongo.repository;
 
 import com.pdetoni.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title': { $regex:  ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
